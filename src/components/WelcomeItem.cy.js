836c8234b4fd1df6552f42b073623709877ec419
n-are-n <naren000000000@gmail.com>
2026-01-25 13:36:24 +0530

    refactor(Sample): remove sample files
    remove generated sample file templates while project creation
    
    AMST-00009

--- src/components/WelcomeItem.cy.js
+++
@@ -1,8 +0,0 @@
-import WelcomeItem from './WelcomeItem.vue'
-
-describe('<WelcomeItem />', () => {
-  it('renders', () => {
-    // see: https://on.cypress.io/mounting-vue
-    cy.mount(WelcomeItem)
-  })
-})
