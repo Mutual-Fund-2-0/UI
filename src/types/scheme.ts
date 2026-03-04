cc180dcfe2cbd3dd3dc99c358bdc1c50ba8621de
n-are-n <naren000000000@gmail.com>
2026-03-04 20:54:15 +0530

    refactor(Scheme): update scheme to match api model
    AMST-00014

--- src/types/scheme.ts
+++ src/types/scheme.ts
@@ -1,9 +1,9 @@
 export interface MutualFundScheme {
-  fundHouse: string
-  schemeType: string
-  schemeCategory?: string | null
-  schemeCode: number
-  schemeName?: string | null
-  isinGrowth?: string | null
-  isinDivReinvestment?: string | null
+  code: number
+  name?: string | null
+  house?: string
+  category?: string | null
+  subCategory?: string | null
+  plan?: string | null
+  type?: string | null
 }
