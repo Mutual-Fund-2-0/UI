5cee58a9b7710c94b81fafd63e9334df3b14255e
srnp <naren000000000@gmail.com>
2026-04-05 11:18:28 +0530

    refactor(Page): update page type
    update page type to generic type. decouple from scheme type.
    
    AMST-00027

--- src/types/page.ts
+++ src/types/page.ts
@@ -1,7 +1,10 @@
-import type { MutualFundScheme } from './scheme'
+/**
+ * A generic wrapper for paginated API responses.
+ * @template T - The type of the items in the paginated list.
+ */
 
-export interface PagedResult {
-  schemes: MutualFundScheme[]
+export interface PagedResult<T> {
+  readonly items: T[]
   totalCount: number
   pageNumber: number
   totalPages: number
